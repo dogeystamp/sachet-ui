22af7ac1a4be839184efe00f621900e1602e3151
dogeystamp <dogeystamp@disroot.org>
2023-06-09 17:19:17 -0400

    index.ts: route things properly

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -2,5 +2,7 @@ import m from "mithril";
 import UserList from "./views/UserList"
 import ShareList from "./views/ShareList"
 
-m.mount(document.body, UserList)
-m.mount(document.body, ShareList)
+m.route(document.body, "/users", {
+	"/users": UserList,
+	"/files": ShareList
+})
