5a057ca642310d20e4432b1fb144f4e02e2326e3
dogeystamp <dogeystamp@disroot.org>
2023-06-13 16:24:18 -0400

    UserWidget: added functionality

--- src/bin/components/UserWidget.ts
+++ src/bin/components/UserWidget.ts
@@ -1,8 +1,12 @@
 import m, { Component } from "mithril";
+import Auth from "../models/Auth";
 
 const UserWidget: Component = {
 	view: () => {
-		return m("t.userwidget-login", m(m.route.Link, {href: "/login"}, "Log in"))
+		return Auth.authenticated ?
+			m("t.userwidget-loggedin", "Logged in as ",
+			  m(m.route.Link, { href: "/users/" + Auth.username }, Auth.username)) :
+			m("t.userwidget-login", m(m.route.Link, { href: "/login" }, "Log in"))
 	}
 }
 
