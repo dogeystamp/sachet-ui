81010306e6561d2d64ea4cf71508dc8ddfc0de83
dogeystamp <dogeystamp@disroot.org>
2023-07-12 15:12:08 -0400

    WIP - components/PermissionWidget: implemented stub

--- src/bin/components/PermissionWidget.ts
+++ src/bin/components/PermissionWidget.ts
@@ -28,21 +28,35 @@ export type PermissionID = keyof typeof permissions
 
 export declare namespace PermissionWidget {
 	interface Attrs {
-		// pass this by reference
-		perms: PermissionID[]
+		// pass these by reference
+		perms: Set<PermissionID>
+		flag: {changed: boolean}
 	}
 }
 
 export const PermissionWidget: Component<PermissionWidget.Attrs> = {
 	view: (vnode) => {
 		if (vnode.attrs.perms === undefined) {
-			vnode.attrs.perms = []
+			vnode.attrs.perms = new Set;
 		}
 		return Object.keys(permissions).map((permId: PermissionID) => {
-			return [
-				m("label.form-label", permissions[permId]),
-				m("input[type=checkbox].form-checkbox"),
-			]
+			return m("li.form-checkbox-li",
+				m("input[type=checkbox].form-checkbox", {
+					checked: vnode.attrs.perms.has(permId),
+					onchange: (e: Event) => {
+						let tg = e.target as HTMLInputElement
+						if (tg != undefined) {
+							if (tg.checked) {
+								vnode.attrs.perms.add(permId)
+							} else {
+								vnode.attrs.perms.delete(permId)
+							}
+						}
+						vnode.attrs.flag.changed = true
+					}
+				}),
+				m("label.form-label", permissions[permId].desc),
+			)
 		})
 	},
 }
--- src/bin/views/Admin.ts
+++ src/bin/views/Admin.ts
@@ -5,13 +5,25 @@ import api from "../services/api"
 
 export declare namespace ServerSettings {
 	interface State {
-		settings: { default_permissions: PermissionID[] }
+		settings: {
+			default_permissions: Set<PermissionID>,
+			flag: {
+				changed: boolean
+			}
+		}
+		rawSettings: { default_permissions: PermissionID[] }
 	}
 	interface Attrs { }
 }
 const ServerSettings: Component<ServerSettings.Attrs, ServerSettings.State> = {
 	oninit: async (vnode) => {
-		vnode.state.settings = await api.request({ url: "/admin/settings" })
+		vnode.state.rawSettings = await api.request({ url: "/admin/settings" })
+		vnode.state.settings = {
+			default_permissions: new Set<PermissionID>(vnode.state.rawSettings.default_permissions),
+			flag: {
+				changed: false
+			},
+		}
 	},
 	view: (vnode) => {
 		const settings = vnode.state.settings
@@ -23,7 +35,15 @@ const ServerSettings: Component<ServerSettings.Attrs, ServerSettings.State> = {
 			m("h2", "Settings"),
 			m("h3", "Anonymous permissions"),
 			m("t", "Allow users that are not logged in to:"),
-			m(PermissionWidget, { perms: settings.default_permissions })
+			m(PermissionWidget, {
+				perms: vnode.state.settings.default_permissions,
+				flag: vnode.state.settings.flag
+			}),
+			m("button.form-button", {
+				onclick: () => {
+				},
+				disabled: !vnode.state.settings.flag.changed
+			}, "Save settings")
 		]
 	}
 }
@@ -33,7 +53,7 @@ const AdminView: Component = {
 			m("h2", "Users"), m(UserList),
 			m("h3", "Create user"),
 			m("t", "TODO"),
-			// m(ServerSettings),
+			//m(ServerSettings),
 		)
 	}
 }
